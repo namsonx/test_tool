bc4f6ba9e40b4b4922ad6a120e7812c8dcb31e42
MAS2HC <MAS2HC@HC-UT40270C.hc.apac.bosch.com>
2016-09-15 10:37:28 +0700

    Added read_xls_data file

---
+++ read_xls_data.cpp
@@ -0,0 +1,13 @@
+/*
+ * read_xls_data.cpp
+ *
+ *  Created on: Sep 15, 2016
+ *      Author: MAS2HC
+ */
+
+#include <iostream>
+#include "read_xls_data.h"
+
+
+
+
---
+++ read_xls_data.h
@@ -0,0 +1,15 @@
+/*
+ * read_xls_data.h
+ *
+ *  Created on: Sep 15, 2016
+ *      Author: MAS2HC
+ */
+
+#ifndef READ_XLS_DATA_H_
+#define READ_XLS_DATA_H_
+
+
+
+
+
+#endif /* READ_XLS_DATA_H_ */
