8f52730c7d039cb79b9209392ef996c4e63848da
MAS2HC <MAS2HC@HC-UT40270C.hc.apac.bosch.com>
2016-09-19 17:44:24 +0700

    connectted to jira

--- connect_jira/connect_jira.py
+++ connect_jira/connect_jira.py
@@ -6,12 +6,31 @@ Created on Sep 15, 2016
 
 from jira import JIRA
 
-print "Jira testing"
 
+def connect_to_jira():
+    
+    print 'Jira testing start'
+    options = {
+           'server': 'http://hcutwrk2040.hc.apac.bosch.com:8090'
+           }
+    #options = {
+    #           'server': 'https://namsonx.atlassian.net'
+    #}
+    
+    jira = JIRA(options, basic_auth= ('mas2hc', 'Klv87-04-03'))
+    #jira = JIRA(options, basic_auth = ('namsonx','Test@123456'))
+    projects = jira.projects()
+    print 'Print project:', projects
+    MT_jira = jira.project('MT')
+    print 'MT project: ', MT_jira 
 
-print "testing"
+    print "Jira testing end"
 
+def main():
+    print 'Calling main function: \n'
+    connect_to_jira()
 
 if __name__ == '__main__':
+    main()
     pass
 
