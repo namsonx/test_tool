80c97b910413b76bcdcce31e91e8de8cdf502494
Linh-PC <Linh-PC@Linh-Tran>
2016-09-24 18:39:14 +0700

    connect successful to Jira

--- connect_jira/connect_jira.py
+++ connect_jira/connect_jira.py
@@ -10,18 +10,18 @@ from jira import JIRA
 def connect_to_jira():
     
     print 'Jira testing start'
-    options = {
-           'server': 'http://hcutwrk2040.hc.apac.bosch.com:8090'
-           }
     #options = {
-    #           'server': 'https://namsonx.atlassian.net'
-    #}
+    #       'server': 'http://hcutwrk2040.hc.apac.bosch.com:8090'
+    #       }
+    options = {
+               'server': 'https://namsonx.atlassian.net'
+    }
     
-    jira = JIRA(options, basic_auth= ('mas2hc', '**********'))
-    #jira = JIRA(options, basic_auth = ('namsonx','Test@123456'))
+    #jira = JIRA(options, basic_auth= ('mas2hc', '**********'))
+    jira = JIRA(options, basic_auth = ('namsonx','Test@123456'))
     projects = jira.projects()
     print 'Print project:', projects
-    MT_jira = jira.project('MT')
+    MT_jira = jira.project('SON')
     print 'MT project: ', MT_jira 
 
     print "Jira testing end test"
